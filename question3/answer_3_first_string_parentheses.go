570e2af657e219eaf2f1973955dbe7fdb080751d
Riansyah R <rrriansyah@gmail.com>
2021-10-16 21:31:03 +0700

    Moved all the answers to respective folders.

--- question3/answer_3_first_string_parentheses.go
+++ question3/answer_3_first_string_parentheses.go
@@ -6,42 +6,42 @@ import (
 )
 
 // findFirstStringInBracket() will find words inside first full parentheses
-func findFirstStringInBracket(str string) string { 
-	if (len(str) == 0) {
+func findFirstStringInBracket(str string) string {
+	if len(str) == 0 {
 		return ""
 	}
 
-	firstBracketIndex := strings.Index(str,"(")
+	firstBracketIndex := strings.Index(str, "(")
 	// No first bracket found
 	if firstBracketIndex == -1 {
 		return ""
 	}
 
-	runes := []rune(str) 
-	wordsAfterFirstBracket := string(runes[firstBracketIndex : len(str)])
+	runes := []rune(str)
+	wordsAfterFirstBracket := string(runes[firstBracketIndex:len(str)])
 
-	closingBracketIndex := strings.Index(wordsAfterFirstBracket,")") 
+	closingBracketIndex := strings.Index(wordsAfterFirstBracket, ")")
 	// No closing bracket found
 	if closingBracketIndex == -1 {
 		return ""
 	}
 
 	runes = []rune(wordsAfterFirstBracket)
-	return string(runes[1 : closingBracketIndex])
+	return string(runes[1:closingBracketIndex])
 }
 
 // alternativeFindFirstStringInBracket() will find words inside first parentheses if we know the string will have no Unicode text (without using rune)
 func alternativeFindFirstStringInBracket(str string) string {
 	// Find opening and closing parenthesis index
-	opIndex := strings.Index(s, "(")
-	closeIndex := strings.Index(s, ")")
+	opIndex := strings.Index(str, "(")
+	closeIndex := strings.Index(str, ")")
 
 	// If either index not found, return empty string
 	if opIndex == -1 || closeIndex == -1 {
 		return ""
 	}
 
-	return s[opIndex+1 : closeIndex]
+	return str[opIndex+1 : closeIndex]
 }
 
 func main() {
@@ -49,12 +49,12 @@ func main() {
 	secondString := "String ini tidak memiliki (bracket penutup"
 	thirdString := "Error: token has expired. (code: 403)"
 
-	fmt.Println(findFirstStringInBracket(firstString)) // will return: tapi bohong, ini string
+	fmt.Println(findFirstStringInBracket(firstString))  // will return: tapi bohong, ini string
 	fmt.Println(findFirstStringInBracket(secondString)) // will return nothing
-	fmt.Println(findFirstStringInBracket(thirdString)) // will return: "code: 403"
+	fmt.Println(findFirstStringInBracket(thirdString))  // will return: "code: 403"
 
 	// Alternatif function without using rune:
-	fmt.Println(alternativeFindFirstStringInBracket(firstString)) // will return: tapi bohong, ini string
+	fmt.Println(alternativeFindFirstStringInBracket(firstString))  // will return: tapi bohong, ini string
 	fmt.Println(alternativeFindFirstStringInBracket(secondString)) // will return nothing
-	fmt.Println(alternativeFindFirstStringInBracket(thirdString)) // will return: "code: 403"
+	fmt.Println(alternativeFindFirstStringInBracket(thirdString))  // will return: "code: 403"
 }
